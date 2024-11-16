de1574a6e2cf144399212e96f4725252eaa747eb
Amin Latifi <a.latifi.al@gmail.com>
2024-11-17 00:30:39 +0700

    Fixed the build issues

--- src/app/eternal/page.tsx
+++ src/app/eternal/page.tsx
@@ -1,12 +1,9 @@
 "use client";
 
-import { Textarea } from "@/components/ui/textarea";
-
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useUnion } from "@/context/UnionContext";
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
 import { useEffect, useState } from "react";
 import { useCivilRegistryContract } from "@/hooks/useContract";
 import { Input } from "@/components/ui/input";
